4c48a4139815cc174882f545808f541cb61012dc
Pierre-Adrien Delisle <pierreadrien.delisle@free.fr>
2019-03-19 19:58:45 +0100

    handRaycast

---
+++ Assets/HandRaycast.cs
@@ -0,0 +1,46 @@
+﻿using Leap;
+using Leap.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandRaycast : MonoBehaviour
+{
+
+    Hand leapHand;
+    FingerModel finger;
+    HandModel handModel;
+
+    // Use this for initialization
+    void Start()
+    {
+        /*Controller controller = new Controller();
+        Frame frame = controller.Frame(); // controller is a Controller object
+        if (frame.Hands.Count > 0)
+        {
+            List<Hand> hands = frame.Hands;
+            leapHand = hands[0];
+        }
+        finger = leapHand.Fingers;
+        Finger.FingerType fingerType = finger.Type;*/
+        handModel = GetComponent<HandModel>();
+        leapHand = handModel.GetLeapHand();
+        if (leapHand == null) Debug.LogError("No leap_hand founded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        for (int i = 0; i < HandModel.NUM_FINGERS; i++)
+        {
+            finger = handModel.fingers[i];
+            if (Physics.Raycast(finger.GetTipPosition(), fwd, out hit,10))
+            {
+                float distanceToGround = hit.distance;
+                Debug.Log("hit something" + distanceToGround);
+            }
+            Debug.DrawRay(finger.GetTipPosition(), finger.GetRay().direction*10, Color.red, Time.deltaTime, true);
+        }
+    }
+}
---
+++ Assets/Pointer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * -10, Color.red);
+        if(Physics.Raycast(transform.position,transform.forward * -1, out hit, 10))
+        {
+			Debug.Log("Le raycast touche un objet !");
+            Debug.Log(hit.transform.tag);
+            if (hit.transform.tag == "Ane")
+            {
+                Debug.Log("Un ANE ! ");
+            }
+        }
+    }
+}
